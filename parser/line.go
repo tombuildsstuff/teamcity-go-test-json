3474d7981cdac2ade0b2cb71e9e856e6f09c71a1
tombuildsstuff <git@ibuildstuff.co.uk>
2019-12-23 15:40:14 +0100

    ignoring go mod output

--- parser/line.go
+++ parser/line.go
@@ -3,9 +3,15 @@ package parser
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
 func ParseLine(input string) (*GoTestJsonLogLine, error) {
+	// e.g. "go: downloading github.com/hashicorp/azure-sdk-for-go [..]"
+	if strings.HasPrefix(input, "go:") {
+		return nil, nil
+	}
+
 	var output GoTestJsonLogLine
 
 	if err := json.Unmarshal([]byte(input), &output); err != nil {
